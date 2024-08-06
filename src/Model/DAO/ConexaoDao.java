37e7e6512ac9ef589e4882ccf1cebc05bf8f9520
Anderson Filho <anderson.filho@parvi.local>
2024-08-06 13:38:36 -0300

    fazendo a conexão de novo

--- src/Model/DAO/ConexaoDao.java
+++ src/Model/DAO/ConexaoDao.java
@@ -3,11 +3,23 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Model.DAO;
-
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+import java.sql.SQLException;
 /**
  *
  * @author filho
  */
 public class ConexaoDao {
-    
+    public Connection conectorBd(){
+        Connection conn = null;
+        
+        try {
+            String  url = "jdbc:mysql://localhost:3306/gaginho?user=root&password=mysql";
+            conn = DriverManager.getConnection(url);
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "ConexaoDao"+ e.getMessage());
+        }return conn;
+    }
 }
